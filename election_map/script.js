28c73c973ab23cd7e0108f59089574563f8fae56
Jimmy <thecoderjimmy@gmail.com>
2019-03-11 08:54:55 +0900

    made factory function for making candidates

--- election_map/script.js
+++ election_map/script.js
@@ -1,4 +1,14 @@
-var politician = {};
-politician.name = "";
+var makePolitician = function (firstName) {
+
+var politician = {}; // new blank object
+politician.name = firstName; // set name to parameter value
 politician.electionResults = null;
 politician.totalVotes = "0";
+
+return politician;
+  
+};
+
+var candidateOne = makePolitician("Hilary");
+var candidateTwo = makePolitician("Donald");
+
